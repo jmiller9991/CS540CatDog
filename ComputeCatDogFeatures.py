289bebbe70f6af348478bcf44658a4f3f98e7183
jmiller9991 <jacobmiller9991@gmail.com>
2020-03-10 01:01:57 -0400

    Change File

---
+++ ComputeCatDogFeatures.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2 as cv
+import tensorflow as tf
+import tensorflow.keras as ks
+import tensorflow.keras.utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import pathlib as pl
+import glob
+
+data_folder_train = 'train/*.jpg'
+data_folder_test = 'test1/*.jpg'
+
+winSize = (200, 200)
+blockSize = (16, 16)
+blockStride = (8, 8)
+cellSize = (8, 8)
+nbins = 9
+
+winSize=(8, 8)
+padding=(8, 8)
+
+def main():
+    filestrain = [cv.imread(file, cv.IMREAD_GRAYSCALE) for file in glob.iglob(data_folder_train)]
+    filestest = [cv.imread(file, cv.IMREAD_GRAYSCALE) for file in glob.iglob(data_folder_test)]
+
+    X_train = []
+    X_test = []
+
+    hog = cv.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
+
+    for i in filestrain:
+        i = cv.resize(i, winSize)
+        hog.compute(i, winSize, padding)
+
+    for i in filestest:
+        i = cv.resize(i, winSize)
+        hog.compute(i, winSize, padding)
+
+
+if __name__ == '__main__':
+    main()
+
+
