5898f6ecf903885b2279f6fb950e8e4e0dc79af0
jmiller9991 <jacobmiller9991@gmail.com>
2020-03-09 22:06:58 -0400

    Initilization

---
+++ CatDog.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as ks
+import tensorflow.keras.utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+
+
+
